09d00f7cecbfead9eab0529a74c781178b05677e
Shah Param Devenbhai <144337259+pdshah09@users.noreply.github.com>
2026-06-17 12:14:18 +0530

    Eslint Fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = defineConfig([
   ]),
   {
     rules: {
+      'react-hooks/set-state-in-effect': 'off',
       "@typescript-eslint/no-explicit-any": "off", // Disables the 'any' error
     },
   },
